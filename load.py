2efea8e8f70da334990eaa0409535e52f4391409
luwi1993 <lwidowski@alessio-analytics.com>
2020-09-04 23:06:34 +0200

    implement load.py

--- load.py
+++ load.py
@@ -1,4 +1,21 @@
 import wave
+import struct
+import matplotlib.pyplot as plt
 
-file = wave.open("files/noise.wav", mode = "rb")
-print(file)
+class WaveLoader:
+    def __init__(self):
+        self.samples = []
+
+    def load(self, path="files/noise.wav"):
+        file = wave.open(path, mode="r")
+        self.samples = [struct.unpack("f", file.readframes(2)) for i in range(int(file.getnframes() / 2))]
+
+    def plot(self, samples=None):
+        if not samples:
+            samples = self.samples
+        plt.plot(samples)
+        plt.show()
+
+w = WaveLoader()
+w.load()
+w.plot()
